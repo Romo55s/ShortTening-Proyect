63879624c828b67574d242987b2275c8f9c2652a
Tony <pp.tony777@gmail.com>
2023-06-26 21:59:10 -0600

    Adding hamburger menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMobileView = false;
+  isMenuOpen = false;
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkView();
+  }
+
+  ngOnInit() {
+    this.checkView();
+  }
+
+  checkView() {
+    this.isMobileView = window.innerWidth <= 375;
+  }
+
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
